e4a27145e8bf2ace400c9813e439d18d87cb541f
efzet <fauzi.padlaw@gmail.com>
2019-03-14 02:56:00 +0700

    surat valid!

--- src/views/VerifyLetter.vue
+++ src/views/VerifyLetter.vue
@@ -57,7 +57,7 @@
             <v-card class="elevation-1 pa-3" v-if="result">
               <v-card-text>
                 <div class="layout column align-center">
-                  <h1 class="flex my-4 primary--text">Surat Terverifikasi!</h1>
+                  <h1 class="flex my-4 primary--text">Surat Valid!</h1>
                 </div>
                 <v-flex sm12>
                   <v-container>
